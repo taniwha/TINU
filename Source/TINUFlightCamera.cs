c21f4c1ed17f194f58fa2e4860d18e19d70f4aa3
Bill Currie <bill@taniwha.org>
2020-04-09 20:37:11 +0900

    Figure out how blender does it
    Roll now works very nicely. Sensitivity could do with a scaling factor,
    but it's acceptable for now.

--- Source/TINUFlightCamera.cs
+++ Source/TINUFlightCamera.cs
@@ -17,7 +17,6 @@ public class TINUFlightCamera : FlightCamera
 	protected override void Start ()
 	{
 		cameraPivot = transform.parent;
-		Debug.Log($"[TINU] {cameraPivot.gameObject.name}");
 	}
 
 	protected override void OnDestroy ()
@@ -28,6 +27,32 @@ public class TINUFlightCamera : FlightCamera
 	Quaternion deltaRotation;
 	bool setRotation;
 
+	const float r = 1;
+	const float t = r * r / 2;
+	// xyVec is already in -1..1 order of magnitude range (ie, might be a bit
+	// over due to screen aspect)
+	// This is very similar to blender's trackball calculation (based on it,
+	// really)
+	Vector3 TrackballVector (Vector3 xyVec)
+	{
+		float d = xyVec.x * xyVec.x + xyVec.y * xyVec.y;
+		Vector3 vec = xyVec;
+
+		if (d < t) {
+			// less than 45 degrees around the sphere from the viewer facing
+			// pole, so map the mouse point to the sphere
+			vec.z = -Mathf.Sqrt (r * r - d);
+		} else {
+			// beyond 45 degrees around the sphere from the veiwer facing
+			// pole, so the slope is rapidly approaching infinity or the mouse
+			// point may miss the sphere entirely, so instead map the mouse
+			// point to the hyperbolic cone pointed towards the viewer. The
+			// cone and sphere are tangential at the 45 degree latitude
+			vec.z = -t / Mathf.Sqrt (d);
+		}
+		return vec;
+	}
+
 	void HandleInput ()
 	{
 		setRotation = false;
@@ -35,16 +60,17 @@ public class TINUFlightCamera : FlightCamera
 			return;
 		}
 		var center = new Vector3 (Screen.width / 2, Screen.height / 2, 0);
-		Vector3 end = (Input.mousePosition - center) / Screen.width;
-		end.z = -Screen.width / 2;
-		end /= Screen.width / 2;
+		Vector3 end = (Input.mousePosition - center);
+		end /= Screen.height / 2;
 		float deltaMx = Input.GetAxis ("Mouse X");
 		float deltaMy = Input.GetAxis ("Mouse Y");
 		var delta = new Vector3 (deltaMx, deltaMy, 0) * orbitSensitivity;
 		Vector3 start = end + delta;
-		end = transform.TransformDirection (end);
-		start = transform.TransformDirection (start);
-		deltaRotation = Quaternion.FromToRotation (start, end);
+		end = TrackballVector (end);
+		start = TrackballVector (start);
+		Vector3 axis = transform.TransformDirection(Vector3.Cross(start, end));
+		float angle = delta.magnitude / (2 * r) * 60;
+		deltaRotation = Quaternion.AngleAxis (angle, axis);
 		setRotation = true;
 	}
 
